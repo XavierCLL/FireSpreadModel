1067809f154f50ef904af74d6e4e1ded9acaba4c
Xavier Corredor Llano <xavier.corredor.llano@gmail.com>
2017-09-04 22:50:29 -0500

    init

---
+++ cellular_automata.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#
+#  Copyright (C) 2017 Xavier Corredor Llano
+#  Email: xavier.corredor.llano <a> gmail.com
+#
+#  This program is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
